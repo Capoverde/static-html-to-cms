1ec631493b86472e4387df71c88516b3119711b3
Piotr <piotr_as@yahoo.com>
2021-07-05 11:50:26 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import '../scss/app.scss'
+/* Demo JS */
+import './demo.js'
+
+/* Your JS Code goes here */
+console.log('%c Hello from app.js', 'color: #ffc600; background: #000; padding: 5px 10px; margin: 5px 25px; font-size:2em;')
---
+++ src/js/demo.js
@@ -0,0 +1,1 @@
+console.log('%c Hello from Demo.js', 'color: #bada55; background: #000; padding: 5px 10px; margin: 5px 25px; font-size:2em;')
